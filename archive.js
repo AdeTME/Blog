04f24f0e3c21288021b0cdade2b88d8e82f3242e
Adetunji Okekunle <tjokekunle@gmail.com>
2026-07-07 13:59:29 +0100

    I was going to do more JS but ive decided to be more html first and only write js code when necessary

--- archive.js
+++ archive.js
@@ -1,1 +1,9 @@
+//this is meant to be the dynamic archives list for all the pages available in the website... 
+// it will pick each posts important information, stored in the postdata.js page and then render them on the corresponding archive.html
+
+
+import postData from "./posts/postdata";
+
+
 const archiveContainer = document.querySelector("#archiveContainer");
+
---
+++ posts/postdata.js
@@ -0,0 +1,17 @@
+//I was going to make the posts render dynamically, but since I want more HTML first code, ive decided against that for now
+//In the event that i make more functionality ill try to make the site more html first
+
+
+const postData = {
+    "post-date": "July 7, 2026",
+    "post-title": "Luigi Mangione: On Martyrdon, or Living On a Corpse",
+    "post-excerpt": "Mangione, despite walking up to a man and shooting him, garnered a lot more sympathy than a man who self- immolated for people thousands of kilometres away....",
+    "link":"posts/post1.html",
+    "content-image":"",
+    "alt":"Luigi Mangione",
+}
+
+
+
+
+export default postData;
