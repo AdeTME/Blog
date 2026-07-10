d4e4b72c09bce756848292e5ed1472b2992d19f9
Adetunji Okekunle <tjokekunle@gmail.com>
2026-07-10 22:38:34 +0100

    I created the tags and can now use dom to see which were clicked

--- archive.js
+++ archive.js
@@ -1,9 +1,8 @@
-//this is meant to be the dynamic archives list for all the pages available in the website... 
-// it will pick each posts important information, stored in the postdata.js page and then render them on the corresponding archive.html
 
+const btns = document.querySelectorAll("li button");
 
-import postData from "./posts/postdata";
-
-
-const archiveContainer = document.querySelector("#archiveContainer");
-
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        console.log(btn.dataset.tag);
+    });
+});
